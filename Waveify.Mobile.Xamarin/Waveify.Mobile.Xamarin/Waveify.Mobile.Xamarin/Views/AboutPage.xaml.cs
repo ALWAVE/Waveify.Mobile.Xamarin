dfb96ff3009f637d457f80d2a08a1c4fec9f8098
ukraincev_artem <aptem.csgo@mail.ru>
2024-12-11 23:02:02 +0400

    first commit

---
+++ Waveify.Mobile.Xamarin/Waveify.Mobile.Xamarin/Waveify.Mobile.Xamarin/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Waveify.Mobile.Xamarin.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Waveify.Mobile.Xamarin/Waveify.Mobile.Xamarin/Waveify.Mobile.Xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Waveify.Mobile.Xamarin.ViewModels;
+using Xamarin.Forms;
+
+namespace Waveify.Mobile.Xamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
