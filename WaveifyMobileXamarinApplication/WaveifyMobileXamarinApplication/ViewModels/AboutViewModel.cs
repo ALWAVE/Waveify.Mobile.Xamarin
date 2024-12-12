2248a1279c198dc329596d41e4497ca19ac05fcf
ukraincev_artem <aptem.csgo@mail.ru>
2024-12-12 22:34:44 +0400

    update ui, add log page + content

--- WaveifyMobileXamarinApplication/WaveifyMobileXamarinApplication/ViewModels/AboutViewModel.cs
+++ WaveifyMobileXamarinApplication/WaveifyMobileXamarinApplication/ViewModels/AboutViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Input;
+using WaveifyMobileXamarinApplication.Views;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -7,12 +8,19 @@ namespace WaveifyMobileXamarinApplication.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        public ICommand LoginPageView { get; private set; }
         public AboutViewModel()
         {
-            Title = "Hi";
+            Title = "Waveify";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            LoginPageView = new Command(OnLoginPageView);
         }
-
         public ICommand OpenWebCommand { get; }
+        private async void OnLoginPageView()
+        {
+            // Здесь вы можете заменить текущую страницу на новую
+            var loginPage = new LoginPage(); // Предполагается, что у вас есть LoginPage
+            await Application.Current.MainPage.Navigation.PushAsync(loginPage);
+        }
     }
 }
