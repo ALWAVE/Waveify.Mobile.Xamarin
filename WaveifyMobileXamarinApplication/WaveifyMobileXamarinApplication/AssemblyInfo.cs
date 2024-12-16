e3327571947c020cbb92c72f21bdfd56b4809625
ukraincev_artem <aptem.csgo@mail.ru>
2024-12-17 02:43:50 +0400

    update ui, add fonts

--- WaveifyMobileXamarinApplication/WaveifyMobileXamarinApplication/AssemblyInfo.cs
+++ WaveifyMobileXamarinApplication/WaveifyMobileXamarinApplication/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("TT Interphases Pro Trial Medium.ttf", Alias = "IPTMedium")]
+[assembly: ExportFont("TT Interphases Pro Trial Regular.ttf", Alias = "IPTRegular")]
+[assembly: ExportFont("TTFirsNeue-Black.ttf", Alias = "FirsNeueBlack")]
+[assembly: ExportFont("TTFirsNeue-Light.ttf", Alias = "FirsNeueLight")]
+[assembly: ExportFont("TTFirsNeue-Medium.ttf", Alias = "FirsNeueMedium")]
